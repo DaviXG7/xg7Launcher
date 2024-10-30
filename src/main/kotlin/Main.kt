8109cc257a0e54581fc69b8f17092c0a30303976
davis <davisonic2010@gmail.com>
2024-10-30 15:30:35 -0300

    InitialCommit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -69,6 +70,7 @@ fun App() {
 fun main() = application {
     Window(onCloseRequest = ::exitApplication,
         state = rememberWindowState(width = 1372.dp, height = 727.dp),
+        icon = painterResource("images/logo.png")
         ) {
         App()
     }
